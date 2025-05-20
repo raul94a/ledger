8f8d8f4c36dc6c7ecd6f90207c9941bbc36da24f
raul.albin <raul200794@gmail.com>
2025-05-20 13:56:40 +0200

    mix db test

--- src/test/database/transaction_test.go
+++ src/test/database/transaction_test.go
@@ -121,21 +121,3 @@ func TestDatabaseConnection(t *testing.T) {
 
 }
 
-package transaction_test
-
-import (
-	"context"
-	"database/sql"
-	"fmt"
-	"log"
-	cliententity "src/domain/client"
-	"src/test/utils"
-	"testing"
-	"time"
-
-	_ "github.com/lib/pq"
-	"github.com/stretchr/testify/assert"
-	"github.com/testcontainers/testcontainers-go"
-	"github.com/testcontainers/testcontainers-go/modules/postgres"
-	"github.com/testcontainers/testcontainers-go/wait"
-)
